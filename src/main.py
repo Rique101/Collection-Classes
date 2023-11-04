6cf03f5cdb4ef361e017f542f949c62966a62476
Rique101 <etejada2347@gmail.com>
2023-11-03 23:28:58 -0400

    Fifth Version of Collection Classes

--- src/main.py
+++ src/main.py
@@ -7,7 +7,105 @@ def main():
     # testRemoveNodeAfter()
     # review()
     #testListLength()
-    testListSearch()
+    #testListSearch()
+    #testListPosition()
+    #testListCopy()
+    testListCopyWithTail()
+
+def testListCopyWithTail():
+    print("Testing List Copy With Tail")
+
+    #Construct a node with data equal to S and link equal to None
+    #and assign its reference to source
+    source = node("S", None) # S
+
+    #Construct a node with data equal to B and link equal to source
+    #and assign its reference to source
+    source = node("B", source) # B -> S
+
+    #Construct a node with data equal to O and link equal to source
+    #and assign its reference to source
+    source = node("O", source) # O -> B -> S
+
+    #Construct a node with data equal to J and link equal to source
+    #and assign its reference to source
+    source = node("J", source) # J -> O -> B -> S
+
+    copy = node.listCopyWithTail(source) 
+    # [J -> O -> B -> S, S]
+
+    print("Source contains", node.listPosition(source, 1).getData(),
+          node.listPosition(source, 2).getData(),
+          node.listPosition(source, 3).getData(),
+          node.listPosition(source, 4).getData())
+    
+    print("Copy head contains", node.listPosition(copy[0], 1).getData(),
+          node.listPosition(copy[0], 2).getData(),
+          node.listPosition(copy[0], 3).getData(),
+          node.listPosition(copy[0], 4).getData())
+
+    print("Copy tail contains", node.listPosition(copy[1], 1).getData())
+
+def testListCopy():
+    print("Testing List Copy")
+
+    #Construct a node with data equal to S and link equal to None
+    #and assign its reference to source
+    source = node("S", None) # S
+
+    #Construct a node with data equal to B and link equal to source
+    #and assign its reference to source
+    source = node("B", source) # B -> S
+
+    #Construct a node with data equal to O and link equal to source
+    #and assign its reference to source
+    source = node("O", source) # O -> B -> S
+
+    #Construct a node with data equal to J and link equal to source
+    #and assign its reference to source
+    source = node("J", source) # J -> O -> B -> S
+
+    copy = node.listCopy(source) # J -> O -> B -> S, but at a different memory location
+
+    print("Source contains", node.listPosition(source, 1).getData(),
+          node.listPosition(source, 2).getData(),
+          node.listPosition(source, 3).getData(),
+          node.listPosition(source, 4).getData())
+    
+    print("Copy contains", node.listPosition(copy, 1).getData(),
+          node.listPosition(copy, 2).getData(),
+          node.listPosition(copy, 3).getData(),
+          node.listPosition(copy, 4).getData())
+
+def testListPosition():
+    print("Testing List Position")
+    
+    #Construct a node with data equal to S and link equal to None
+    #and assign its reference to head
+    head = node("S", None) # S
+
+    #Construct a node with data equal to B and link equal to Head
+    #and assign its reference to head
+    head = node("B", head) # B -> S
+
+    #Construct a node with data equal to O and link equal to Head
+    #and assign its reference to head
+    head = node("O", head) # O -> B -> S
+
+    #Construct a node with data equal to J and link equal to Head
+    #and assign its reference to head
+    head = node("J", head) # J -> O -> B -> S
+
+    print('First node contains data:', node.listPosition(head, 1).getData()) # J -> O -> B -> S
+    print('Second node contains data:', node.listPosition(head, 2).getData()) # O -> B -> S
+    print('Third node contains data:', node.listPosition(head, 3).getData()) # B -> S
+    print('Fourth node contains data:', node.listPosition(head, 4).getData()) # S
+
+    if(node.listPosition(head, 5) != None):
+        print("Fifth node contains data:", node.listPosition(head, 5).getData())
+    else:
+        print("There is no fifth node.")
+
 
 def testListSearch():
     print("Testing List Search")
